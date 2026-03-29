ceb8b3b0162f151331a96bafc236c63a287fce89
Tomáš Kanský <tomator5000@gmail.com>
2026-03-29 22:51:45 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,37 @@
+import os
+import sys
+import subprocess
+import base64
+from flask import Flask, render_template
+
+def resource_path(relative_path):
+    if hasattr(sys, '_MEIPASS'):
+        return os.path.join(sys._MEIPASS, relative_path)
+    return os.path.join(os.path.abspath("."), relative_path)
+
+app = Flask(__name__, template_folder=resource_path('templates'))
+
+def execute_ps(command):
+    b64_cmd = base64.b64encode(command.encode('utf-16le')).decode('utf-8')
+    inner_args = f"'-NoProfile', '-EncodedCommand', '{b64_cmd}'"
+    full_command = f'powershell -NoProfile -Command "Start-Process powershell -ArgumentList {inner_args} -Verb RunAs"'
+    subprocess.run(full_command, shell=True)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/off')
+def internet_off():
+    ps_script = "Remove-NetFirewallRule -DisplayName 'BlockInternet' -ErrorAction SilentlyContinue; New-NetFirewallRule -DisplayName 'BlockInternet' -Direction Outbound -Action Block -RemoteAddress Internet"
+    execute_ps(ps_script)
+    return "Command sent: Disable"
+
+@app.route('/on')
+def internet_on():
+    ps_script = "Remove-NetFirewallRule -DisplayName 'BlockInternet' -ErrorAction SilentlyContinue"
+    execute_ps(ps_script)
+    return "Command sent: Enable"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
